5d8d8e39811d8ff7058da5476004154ef3a97db2
Neil Penman <neilpenman@gmail.com>
2023-08-12 13:56:05 +1000

    Add score to sentiment

--- amazon/src/org/smap/notifications/interfaces/SentimentProcessing.java
+++ amazon/src/org/smap/notifications/interfaces/SentimentProcessing.java
@@ -7,10 +7,13 @@ import com.amazonaws.services.comprehend.AmazonComprehend;
 import com.amazonaws.services.comprehend.AmazonComprehendClient;
 import com.amazonaws.services.comprehend.model.DetectSentimentRequest;
 import com.amazonaws.services.comprehend.model.DetectSentimentResult;
+import com.amazonaws.services.comprehend.model.SentimentScore;
 import com.amazonaws.services.translate.AmazonTranslate;
 import com.amazonaws.services.translate.AmazonTranslateClient;
 import com.amazonaws.services.translate.model.TranslateTextRequest;
 import com.amazonaws.services.translate.model.TranslateTextResult;
+
+import model.Sentiment;
 
 /*****************************************************************************
  * 
@@ -31,25 +34,45 @@ public class SentimentProcessing extends AWSService {
 	/*
 	 * Get the Sentiment
 	 */
-	public String getSentiment(
+	public Sentiment getSentiment(
 			String source,
 			String language) throws Exception {
-			
+		
+		Sentiment sentiment = new Sentiment();
+		
 		AmazonComprehend comprehend = AmazonComprehendClient.builder()
                 .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
                 .withRegion(region)
                 .build();
 		
-
 		DetectSentimentRequest request = new DetectSentimentRequest()
 				.withText(source)
 				.withLanguageCode(language);
 		DetectSentimentResult result  = comprehend.detectSentiment(request);
 			
-		String sentiment = result.getSentiment();
+		sentiment.sentiment = result.getSentiment();
+		sentiment.score = getScore(result.getSentimentScore(), sentiment.sentiment);
         
 		log.info("Sentiment: " + sentiment);
 		return sentiment;
 		
+	}
+	
+	private Float getScore(SentimentScore scores, String sentiment) {
+		Float score = (float) 0.0;
+		
+		if(sentiment != null) {
+			if(sentiment.equals("POSITIVE")) {
+				score = scores.getPositive();
+			} else if(sentiment.equals("NEGATIVE")) {
+				score = scores.getNegative();
+			} else if(sentiment.equals("NEUTRAL")) {
+				score = scores.getNeutral();
+			} else if(sentiment.equals("MIXED")) {
+				score = scores.getMixed();
+			}
+			
+		}
+		return score;
 	}
 }
